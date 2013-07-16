086e7afd54f98fb5dc58d088f746ecd3dd5ac58d
Kevin Rice <kevin.j.rice@gmail.com>
2013-07-16 02:19:32 -0500

    initial push

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+import web
+
+urls = (
+          '/(.*)', 'index'
+    )
+
+class index:
+    def GET(self, name):
+      return "Hello, World"
+
+app = web.application(urls, globals())
+application = app.wsgifunc()
